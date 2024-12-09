771bce05bfec1f8560934d9fd53babb8a03db184
Carine-bahaa <149932527+Carine-bahaa@users.noreply.github.com>
2024-12-09 17:28:58 +0200

    hey

---
+++ OOP Project/OOP Project/src/Main.java
@@ -0,0 +1,423 @@
+import filehandlingexample.FileHandling;
+
+import javax.print.Doc;
+import javax.xml.crypto.Data;
+import java.io.*;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.List;
+import static filehandlingexample.FileHandling.*;
+
+class patientControl
+{
+
+    public static void removeDoctor(String username)
+    {
+        int size= data.Doctors.size();
+        for (int i = 0; i <size; i++)
+        {
+            if(data.Doctors.get(i).getUsername().equals(username))
+            {
+                data.Doctors.remove(i);
+                break;
+            }
+        }
+    }
+
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        data.Load();
+
+        //Login and Sign Up
+
+        System.out.println("Welcome to the Dental Clinic System Management\n " +
+                "Choose If ypu are a Patient , Doctor or Receptionist\n" +
+                "1) Doctor \n" +
+                "2) Patient \n" +
+                "3) Receptionist \n");
+
+        Scanner input = new Scanner(System.in);
+        int idetinty = input.nextInt();
+
+        if (idetinty == 1) {
+            String username;
+            String password;
+            do {
+                System.out.println("Enter Your UserName and Password : \n ");
+                username = input.next();
+                password = input.next();
+                if (Doctor.login(username, password)) {
+                    System.out.println("logged in successfully\n ");
+
+                } else {
+                    System.out.println("wrong password or username \n " + "Don't You have an Account?\n " +
+                "1)Enter Username and password Again\n" + "2)Sign Up\n");
+                    int choice = input.nextInt();
+                    if( choice == 1)
+                    {
+                        continue;
+                    }
+                    else {
+                        System.out.println("Enter your details:");
+                        System.out.print("Username: ");
+                        String d_username = input.next();
+                        System.out.print("Password: ");
+                        String d_password = input.next();
+                        System.out.print("First Name: ");
+                        String d_firstName = input.next();
+                        System.out.print("Last Name: ");
+                        String d_lastName = input.next();
+                        System.out.print("Email: ");
+                        String d_email = input.next();
+                        System.out.print("Phone Number: ");
+                        String d_phone = input.next();
+                        String [] d_availableDays = new String[3];
+                        ArrayList<int[]> d_availableHours = new ArrayList<>();
+                        for (int i =0 ;i < 3 ;i++) {
+                            System.out.println("Enter Day");
+                            d_availableDays[i] = input.next();
+                            System.out.println("Enter available time (Start End, in hours, e.g., 9 17):");
+                            int start = input.nextInt(); // Get start time
+                            int end = input.nextInt();   // Get end time
+
+                            d_availableHours.add(new int[]{start, end});
+                        }
+                        System.out.println("Specialization: ");
+                       String d_specialization = input.next();
+                               Doctor doctor = new Doctor(d_username, d_password, d_firstName, d_lastName, d_email,
+                                       d_phone, d_availableDays, d_availableHours, d_specialization);
+                        data.Doctors.add(doctor);
+                        System.out.println("You are now signed up");
+
+                        break;
+                    }
+                }
+            } while (!Doctor.login(username, password));
+
+
+
+        } else if (idetinty == 2) {
+
+            String username;
+            String password;
+            do {
+                System.out.println("Enter Your UserName and Password : \n ");
+                username = input.next();
+                password = input.next();
+                if (Patient.login(username, password)) {
+                    System.out.println("logged in successfully\n ");
+
+                } else {
+                    System.out.println("wrong password or username \n " + "Don't You have an Account?\n " +
+                            "1)Enter Username and password Again\n" + "2)Sign Up\n");
+                    int choice = input.nextInt();
+                    if( choice == 1)
+                    {
+                        continue;
+                    }
+                    else {
+                        System.out.println("Enter your details:");
+                        System.out.print("Username: ");
+                        String p_username = input.next();
+                        System.out.print("Password: ");
+                        String p_password = input.next();
+                        System.out.print("First Name: ");
+                        String p_firstName = input.next();
+                        System.out.print("Last Name: ");
+                        String p_lastName = input.next();
+                        System.out.print("Gender : ");
+                        String p_Gender = input.next();
+                        System.out.print("Email: ");
+                        String p_email = input.next();
+                        System.out.print("Phone Number: ");
+                        String p_phone = input.next();
+                        System.out.print("weight: ");
+                        float p_weight = input.nextFloat();
+                        System.out.print("Height: ");
+                        float p_Height = input.nextFloat();
+                        System.out.print("age : ");
+                        int p_age = input.nextInt();
+                        System.out.print("Blood type: ");
+                        String p_bloodtype = input.next();
+
+                        String[] patientHistory = new String[10];
+                        int count = 0; // use it in displaying the describtion
+                        String detail;
+                        for (int i = 0; i < 10; i++) {
+
+                            if (i == 0) {
+                                System.out.print("Enter the next history detail (or press Enter to finish): ");
+                                detail = input.nextLine(); // Read the entire line
+                            } else {
+
+                                detail = input.nextLine(); // Read the entire line
+
+
+                                if (i > 0 && detail.isEmpty()) { // Allow empty input only after the first entry
+                                    break; // Exit the loop
+                                }
+                            }
+
+
+                            patientHistory[i] = detail;
+                            count++;
+                        }
+
+                        Patient patient = new Patient(p_username, p_password, p_firstName, p_lastName, p_email,
+                                p_phone, patientHistory, p_age, p_Gender, p_bloodtype, p_weight, p_Height);
+                        data.Patients.add(patient);
+                        System.out.println("You are now signed up");
+
+                        break;
+                    }
+
+                }
+            } while (!Patient.login(username, password));
+            int action;
+            do{
+                System.out.println("Choose an option \n" +
+                        "1. Update information \n" +
+                        "2. Reserve an Appointment \n" +
+                        "3. Cancel Appointment \n" +
+                        "4. Exit \n" );
+                action = input.nextInt();
+                switch (action)
+                {
+                    case 1:
+                        System.out.println("What do you like to Update: \n" +
+                                "1. Update Email \n" +
+                                "2. Update Mobile number \n" +
+                                "3. Update Weight \n" +
+                                "4. Update Height \n");
+
+                        int update = input.nextInt();
+
+                        if( update == 1)
+                        {
+                            System.out.println("Enter your current Email: ");
+                            String currentEmail = input.next();
+                            System.out.println("Enter your new Email: ");
+                            String newEmail = input.next();
+
+                            Patient.changeEmail( currentEmail,newEmail );
+                        }
+                        else if(update==2)
+                        {
+                            System.out.println("Enter your current Mobile Nmber: ");
+                            String currentMobilenum = input.next();
+                            System.out.println("Enter your new Moble Number: ");
+                            String newMobileNum = input.next();
+
+                            Patient.changeEmail(currentMobilenum ,newMobileNum );
+                        }
+                        else if (update==3)
+                        {
+                            System.out.println("Enter your current Weight: ");
+                            String currentWeight = input.next();
+                            System.out.println("Enter your newWeight: ");
+                            String newWeight = input.next();
+
+                            Patient.changeEmail(currentWeight ,newWeight);
+                        }
+                        else if (update==4)
+                        {
+                            System.out.println("Enter your current Height: ");
+                            String currentHeight = input.next();
+                            System.out.println("Enter your newHeight: ");
+                            String newHeight = input.next();
+
+                            Patient.changeEmail(currentHeight ,newHeight);
+                        }
+                        break;
+
+                    case 2:
+                        System.out.println("Enter doctor's name or mobile number:");
+                        String doctorData = input.next();
+                        System.out.println("Enter day: ");
+                        String requestedDay = input.next();
+                        System.out.println("Enter timeslot ( e.g. 10 ): ");
+                        String requestedTimeSlot = input.next();
+                        System.out.println("Enter your name: ");
+                        String patientName = input.next();
+
+                        boolean doctorFound = false;
+
+                        for (Doctor doctor : data.Doctors) {
+                            if (doctor.getFirstName().equals(doctorData) || doctor.getMobileNumber().equals(doctorData)) {
+                                doctorFound = true;
+
+                                AppointmentReservation appointment = new AppointmentReservation();
+                                appointment.BookAppointment(requestedDay, requestedTimeSlot, doctor, patientName);
+
+                                // Add the appointment to the data list if successfully booked
+                                if (appointment.is_booked) {
+                                    data.Appointments.add(appointment);
+                                }
+                                break;
+                            }
+                        }
+
+                        if (!doctorFound) {
+                            System.out.println("Doctor not found. Please check the details and try again.");
+                        }
+                        break;
+
+                    case 3:
+                        System.out.println("Enter your name:");
+                        String cancelPatientName = input.next();
+
+                        AppointmentReservation CancelAppointment = new AppointmentReservation();
+                        CancelAppointment .CancelAppointment(cancelPatientName);
+
+                        break;
+                    case 4:
+                        break;
+                    default:
+                        System.out.println("Invalid action !!! \n" +
+                                "Please choose again \n");
+                }
+            }while(action !=1 || action !=2|| action !=3 );
+
+
+
+        } else if (idetinty == 3) {
+            String username;
+            String password;
+            do {
+                System.out.println("Enter Your UserName and Password : \n ");
+                username = input.next();
+                password = input.next();
+                if (Receptionist.login(username, password)) {
+                    System.out.println("logged in successfully\n ");
+
+                } else {
+                    System.out.println("wrong password or username \n " + "Don't You have an Account?\n " +
+                            "1)Enter Username and password Again\n" + "2)Sign Up\n");
+                    int choice = input.nextInt();
+                    if (choice == 1) {
+                        continue;
+                    } else {
+                        System.out.println("Enter your details:");
+                        System.out.print("Username: ");
+                        String r_username = input.next();
+                        System.out.print("Password: ");
+                        String r_password = input.next();
+                        System.out.print("First Name: ");
+                        String r_firstName = input.next();
+                        System.out.print("Last Name: ");
+                        String r_lastName = input.next();
+                        System.out.print("Gender : ");
+                        String r_Gender = input.next();
+                        System.out.print("Email: ");
+                        String r_email = input.next();
+                        System.out.print("Phone Number: ");
+                        String r_phone = input.next();
+                        System.out.print("age : ");
+                        int r_age = input.nextInt();
+
+                        Receptionist receptionist= new Receptionist(r_username, r_password, r_firstName, r_lastName, r_email,
+                                r_phone, r_age, r_Gender);
+                        data.Receptionists.add(receptionist);
+                        System.out.println("You are now signed up");
+
+                        break;
+                      }
+                    }
+                }while (!Receptionist.login(username, password)) ;
+
+            int action;
+            do{
+                System.out.println("Choose an option \n" +
+                        "1. Update information \n" +
+                        "2. Reserve an Appointment \n" +
+                        "3. Cancel Appointment \n" +
+                        "4. Exit" );
+                action = input.nextInt();
+                switch (action) {
+                    case 1:
+                        System.out.println("What do you like to Update: \n" +
+                                "1. Update Email \n" +
+                                "2. Update Mobile number \n" );
+
+                        int update = input.nextInt();
+
+                        if( update == 1)
+                        {
+                            System.out.println("Enter your current Email: ");
+                            String currentEmail = input.next();
+                            System.out.println("Enter your new Email: ");
+                            String newEmail = input.next();
+
+                            Receptionist.changeEmail( currentEmail,newEmail );
+                        }
+                        else if(update==2)
+                        {
+                            System.out.println("Enter your current Mobile Nmber: ");
+                            String currentMobilenum = input.next();
+                            System.out.println("Enter your new Moble Number: ");
+                            String newMobileNum = input.next();
+
+                            Receptionist.changeEmail(currentMobilenum ,newMobileNum );
+                        }
+
+                        break;
+                    case 2:
+                        System.out.println("Enter doctor's name or mobile number:");
+                        String doctorData = input.next();
+                        System.out.println("Enter day: ");
+                        String requestedDay = input.next();
+                        System.out.println("Enter timeslot ( e.g. 10 ): ");
+                        String requestedTimeSlot = input.next();
+                        System.out.println("Enter Patient name: ");
+                        String patientName = input.next();
+
+                        boolean doctorFound = false;
+
+                        for (Doctor doctor : data.Doctors) {
+                            if (doctor.getFirstName().equals(doctorData) || doctor.getMobileNumber().equals(doctorData)) {
+                                doctorFound = true;
+
+                                AppointmentReservation appointment = new AppointmentReservation();
+                                appointment.BookAppointment(requestedDay, requestedTimeSlot, doctor, patientName);
+
+                                // Add the appointment to the data list if successfully booked
+                                if (appointment.is_booked) {
+                                    data.Appointments.add(appointment);
+                                }
+                                break;
+                            }
+                        }
+
+                        if (!doctorFound) {
+                            System.out.println("Doctor not found. Please check the details and try again.");
+                        }
+                        break;
+                    case 3:
+                        System.out.println("Enter your name:");
+                        String cancelPatientName = input.next();
+
+                        AppointmentReservation CancelAppointment = new AppointmentReservation();
+                        CancelAppointment .CancelAppointment(cancelPatientName);
+                         break;
+                    case 4:
+                        break;
+                    default:
+                        System.out.println("Invalid action !!! \n" +
+                                "Please choose again \n");
+
+
+                }
+                }while(action !=1 || action !=2|| action !=3 );
+        }
+
+
+
+        data.Store();
+    }
+}
